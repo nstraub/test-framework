eb9cf47f82fbebb276377a974cd10074b4d82534
Kevin Kuszyk <kevin@kevinkuszyk.com>
2014-05-16 11:39:44 +0100

    Bump version to beta2

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta2")]
 [assembly: AssemblyProduct("Test Framework")]
