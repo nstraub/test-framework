3dbe03e444c6ca015e1f04e29b662e75819f68d6
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-10-19 17:24:59 +0100

    Bump version to 1.1

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Kevin Kuszyk")]
-[assembly: AssemblyCopyright("Copyright Kevin Kuszyk 2014")]
+[assembly: AssemblyCopyright("Copyright Kevin Kuszyk 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 [assembly: AssemblyProduct("Test Framework")]
