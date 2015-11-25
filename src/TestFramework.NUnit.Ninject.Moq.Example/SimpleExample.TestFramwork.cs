4b7fb5ca1c5d3a06e196d444f5394b0fab48661e
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-11-25 09:31:24 +0000

    Add simple example from ninject mocking kernel wiki

---
+++ src/TestFramework.NUnit.Ninject.Moq.Example/SimpleExample.TestFramwork.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace TestFramework.NUnit.Ninject.Moq.Example.TestFramework
+{
+    [TestFixture]
+    public class MyTests : TestsFor<MyFoo>
+    {
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            // setup the mock
+            var barMock = GetMock<IBar>();
+            barMock.Setup(mock => mock.GetContent()).Returns("mocked Content");
+
+            // Act
+            Subject.DoStuff(); // The Subject property gets the real MyFoo with a mocked IBar injected into it
+                               // this will print "mocked Content", because the mocked object is called
+
+            // Assert
+            barMock.VerifyAll();
+        }
+    }
+}
---
+++ src/TestFramework.NUnit.Ninject.Moq.Example/SimpleExample.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Ninject;
+using Ninject.MockingKernel.Moq;
+using NUnit.Framework;
+
+namespace TestFramework.NUnit.Ninject.Moq.Example
+{
+    public interface IBar
+    {
+        string GetContent();
+    }
+    public interface IFoo
+    {
+        void DoStuff();
+    }
+
+    public class MyFoo : IFoo
+    {
+        private readonly IBar _bar;
+        public MyFoo(IBar bar)
+        {
+            _bar = bar;
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine(_bar.GetContent());
+        }
+    }
+
+
+    [TestFixture]
+    public class MyTests
+    {
+        private readonly MoqMockingKernel _kernel;
+
+        public MyTests()
+        {
+            _kernel = new MoqMockingKernel();
+            _kernel.Bind<IFoo>().To<MyFoo>();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _kernel.Reset();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //setup the mock
+            var barMock = _kernel.GetMock<IBar>();
+            barMock.Setup(mock => mock.GetContent()).Returns("mocked Content");
+
+            var foo = _kernel.Get<IFoo>(); // this will inject the mocked IBar into our normal MyFoo implementation
+            foo.DoStuff();  // this will print "mocked Content", because the mocked object is called
+
+            barMock.VerifyAll();
+        }
+    }
+
+}
